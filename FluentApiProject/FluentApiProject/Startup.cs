d8b47eb7fa0919c86e6deea26f69f17da0f46e14
ExpertPractice <expertpractice@gmail.com>
2018-03-04 12:28:53 +0500

    Everything

---
+++ FluentApiProject/FluentApiProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FluentApiProject.Startup))]
+namespace FluentApiProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
